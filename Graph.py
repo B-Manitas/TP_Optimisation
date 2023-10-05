b20456ba907aa9d29ed46cf53dfa39c060fec2b1
Manitas Bahri <manitas.bhr@gmail.com>
2023-10-05 17:51:00 +0200

    fix: fix bug when searching the minimum delta refactor: make find_shorter_path method return the shorter path and the distance

--- Graph.py
+++ Graph.py
@@ -22,26 +22,38 @@ class Graph:
     def find_shorter_path(self) -> dict:
         first_node = self.df_adjacency.index[0]
         delta = {first_node: 0}
+        shorter_path = {first_node: first_node}
+
+        # Set of all nodes. (Equivalent to M in the TP).
         set_of_nodes = set(self.df_adjacency.index)
+
+        # Set of visited nodes. (Equivalent to X in the TP).
         set_of_visited_nodes = {first_node}
 
         while set_of_visited_nodes != set_of_nodes:
-            node_not_visited = set_of_nodes - set_of_visited_nodes
-            j = None
+            node_j = None
 
-            for node in node_not_visited:
+            # For each node in node_not_visited, check if all previous nodes are in set_of_visited_nodes
+            for node in set_of_nodes - set_of_visited_nodes:
                 if self.get_previous_nodes_set(node) - set_of_visited_nodes == set():
-                    j = node
+                    node_j = node
                     break
 
-            delta[j] = np.inf
-            for i in self.get_previous_nodes_set(j):
-                # delta[i] exist because i is a previous node of j
-                a = delta[i] + self.df_adjacency[j][i]
+            # Find the minimum delta[node_j] and the node_i that minimize the distance
+            delta[node_j] = np.inf
+            min_node = None
 
-                if delta[j] > a:
-                    delta[j] = a
-                    set_of_visited_nodes.add(j)
-                    break
+            for node_i in self.get_previous_nodes_set(node_j):
 
-        return delta
+                # delta[node_i] exist because node_i is a previous node of j
+                min_delta = delta[node_i] + self.df_adjacency[node_j][node_i]
+
+                # Find the minimum distance
+                if delta[node_j] > min_delta:
+                    delta[node_j] = min_delta
+                    min_node = node_i
+
+            set_of_visited_nodes.add(node_j)
+            shorter_path[node_j] = min_node
+
+        return delta, shorter_path
